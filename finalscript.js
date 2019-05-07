5bb43c6ecc28be2bf2649ff6e9d937c6c4b931ea
livbonner <46944727+livbonner@users.noreply.github.com>
2019-05-07 18:06:05 -0400

    Add files via upload

---
+++ finalscript.js
@@ -0,0 +1,17 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems); 
+});
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
+  });
+
+        
